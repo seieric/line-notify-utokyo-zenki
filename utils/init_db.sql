b6802bf125d9687c004b3f8bfb3b0526d65deb78
seieric <30688855+seieric@users.noreply.github.com>
2024-02-11 21:29:02 +0900

    remove: Prisma搭載で不要になったファイル

--- utils/init_db.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS line_notify_tokens (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  token VARCHAR(255) NOT NULL,
-  access_time TIMESTAMP NOT NULL,
-  ip_address VARCHAR(45) NOT NULL,
-  user_agent VARCHAR(255) NOT NULL
-);
-
-ALTER TABLE line_notify_tokens ADD notify_type INT NOT NULL DEFAULT 0;
