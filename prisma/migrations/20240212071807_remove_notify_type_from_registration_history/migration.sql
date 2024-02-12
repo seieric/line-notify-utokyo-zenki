952a814031b14a80ed11a138a706065858065b41
seieric <30688855+seieric@users.noreply.github.com>
2024-02-12 16:18:47 +0900

    fix: remove notify_type from RegistrationHistory

---
+++ prisma/migrations/20240212071807_remove_notify_type_from_registration_history/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `notify_type` on the `RegistrationHistory` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `RegistrationHistory` DROP COLUMN `notify_type`;
