223dbdc2ab67557b4ccd84799328bdbb18207d6f
seieric <30688855+seieric@users.noreply.github.com>
2024-02-12 14:22:24 +0900

    refactor

--- src/app.ts
+++ src/app.ts
@@ -23,12 +23,19 @@ const prisma = new PrismaClient();
 
 const ENV_PATH = path.join(__dirname, "/../.env");
 dotenv.config({ path: ENV_PATH });
+
+function generateRandomString() {
+  return crypto.randomBytes(20).toString("hex");
+}
+
+// initialize fastify
 const app = fastify({
   logger: true,
   trustProxy: "127.0.0.1",
 }).withTypeProvider<TypeBoxTypeProvider>();
-const port = parseInt(process.env.PORT || "3000");
 
+// session
+// redis
 const redisClient = new Redis(
   process.env.REDIS_URL || "redis://localhost:6379"
 );
@@ -36,17 +43,7 @@ redisClient.on("error", (error) => {
   console.error(error);
   process.exit(1);
 });
-
-declare module "@fastify/session" {
-  interface FastifySessionObject {
-    state: string | undefined;
-    csrfToken: string;
-    tokenId: number;
-    token: string;
-  }
-}
-
-// session
+// @fastify/session
 app.register(fastifyCookie);
 app.register(fastifySession, {
   cookieName: "session",
@@ -59,8 +56,17 @@ app.register(fastifySession, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     sameSite: "lax",
-  }
+  },
 });
+declare module "@fastify/session" {
+  interface FastifySessionObject {
+    state: string | undefined;
+    csrfToken: string;
+    tokenId: number;
+    token: string;
+  }
+}
+
 // view engine
 app.register(fastifyView, {
   engine: {
@@ -69,17 +75,15 @@ app.register(fastifyView, {
   root: path.join(__dirname, "../views"),
 });
 
-function generateRandomString() {
-  return crypto.randomBytes(20).toString("hex");
-}
-
 // publicディレクトリを公開
 app.register(fastifyStatic, {
   root: path.join(__dirname, "../public"),
 });
 
+// x-www-form-urlencodedに対応
 app.register(fastifyFormBody);
 
+// カスタムエラーページ
 app.setErrorHandler((error, req, res) => {
   req.log.error(error);
   res.status(error.statusCode || 500).view("error", {
@@ -87,6 +91,7 @@ app.setErrorHandler((error, req, res) => {
   });
 });
 
+// routes
 app.get("/", (req, res) => {
   res.view("index.pug");
 });
@@ -106,162 +111,166 @@ app.get("/auth", (req, res) => {
   res.redirect(`https://notify-bot.line.me/oauth/authorize?${params}`);
 });
 
-const callback_schema = {
-  schema: {
-    querystring: Type.Union([
-      Type.Object({
-        error: Type.String(),
-      }), 
-      Type.Object({
-        code: Type.String(),
-        state: Type.String(),
-      })
-    ]),
+app.get(
+  "/callback",
+  {
+    schema: {
+      querystring: Type.Union([
+        Type.Object({
+          error: Type.String(),
+        }),
+        Type.Object({
+          code: Type.String(),
+          state: Type.String(),
+        }),
+      ]),
+    },
   },
-};
+  async (req, res) => {
+    if ("error" in req.query) {
+      return res.view("finish", {
+        title: "連携エラー",
+        message:
+          "認証連携が中断されました。本サービスに登録したい場合は、もう一度トップページから進んでください。",
+      });
+    } else {
+      if (req.query.state !== req.session.state) {
+        console.log("Invalid state:", req.query.state, req.session.state);
+        req.session.state = undefined;
+        return res.status(400).view("error", {
+          message: "Invalid request",
+        });
+      }
 
-app.get("/callback", callback_schema, async (req, res) => {
-  if ("error" in req.query) {
-    return res.view("finish", {
-      title: "連携エラー",
-      message:
-        "認証連携が中断されました。本サービスに登録したい場合は、もう一度トップページから進んでください。",
-    });
-  } else {
-    if (req.query.state !== req.session.state) {
-      console.log("Invalid state:", req.query.state, req.session.state);
+      // 2回目からのアクセスはstateを削除
       req.session.state = undefined;
+
+      try {
+        const tokenResponse = await axios.post(
+          "https://notify-bot.line.me/oauth/token",
+          querystring.stringify({
+            code: req.query.code,
+            client_id: process.env.LINE_NOTIFY_CLIENT_ID,
+            client_secret: process.env.LINE_NOTIFY_CLIENT_SECRET,
+            redirect_uri: process.env.REDIRECT_URI,
+            grant_type: "authorization_code",
+          }),
+          {
+            headers: {
+              "Content-Type": "application/x-www-form-urlencoded",
+            },
+          }
+        );
+
+        const accessToken = tokenResponse.data.access_token;
+        console.log("LINE Notify Access Token:", accessToken);
+        // データベースにトークンと関連情報を保存
+        const result = await prisma.line_notify_tokens.create({
+          data: {
+            token: accessToken,
+            access_time: new Date(),
+            ip_address: req.ip || "", // Set a default value of an empty string if req.ip is undefined
+            user_agent: req.headers["user-agent"] || "",
+          },
+        });
+
+        req.session.tokenId = result.id;
+        req.session.token = accessToken;
+
+        // CSRFトークンを生成
+        const csrfToken = generateRandomString();
+        req.session.csrfToken = csrfToken;
+
+        return res.view("select.pug", {
+          title: "通知内容の選択",
+          message:
+            "通知を受け取りたいお知らせの学年を選択してください。本ページで回答せずに閉じると、すべてのお知らせが通知されます。",
+          csrfToken: csrfToken,
+        });
+      } catch (error) {
+        console.error("Error saving token to database: " + error);
+        return res.status(500).view("finish.pug", {
+          title: "エラー",
+          message:
+            "連携情報を保存するのに失敗しました。しばらく時間をおいてもう一度お試しください。",
+        });
+      }
+    }
+  }
+);
+
+app.post(
+  "/finish",
+  {
+    schema: {
+      body: Type.Object({
+        notify_type: Type.Enum(NotifyType),
+        _csrf: Type.String(),
+      }),
+    },
+  },
+  async (req, res) => {
+    if (req.body._csrf !== req.session.csrfToken) {
       return res.status(400).view("error", {
-        message: "Invalid request",
+        message: "CSRF verification failed",
       });
     }
 
-    // 2回目からのアクセスはstateを削除
-    req.session.state = undefined;
+    if (req.session.tokenId === undefined || req.session.token === undefined) {
+      return res.status(400).view("error", {
+        message: "Invalid session",
+      });
+    }
 
+    const notifyType = req.body.notify_type;
     try {
-      const tokenResponse = await axios.post(
-        "https://notify-bot.line.me/oauth/token",
-        querystring.stringify({
-          code: req.query.code,
-          client_id: process.env.LINE_NOTIFY_CLIENT_ID,
-          client_secret: process.env.LINE_NOTIFY_CLIENT_SECRET,
-          redirect_uri: process.env.REDIRECT_URI,
-          grant_type: "authorization_code",
-        }),
-        {
-          headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-          },
-        }
-      );
-
-      const accessToken = tokenResponse.data.access_token;
-      console.log("LINE Notify Access Token:", accessToken);
-      // データベースにトークンと関連情報を保存
-      const result = await prisma.line_notify_tokens.create({
+      // 通知設定を反映
+      await prisma.line_notify_tokens.update({
+        where: {
+          id: req.session.tokenId,
+        },
         data: {
-          token: accessToken,
-          access_time: new Date(),
-          ip_address: req.ip || "", // Set a default value of an empty string if req.ip is undefined
-          user_agent: req.headers["user-agent"] || "",
+          notify_type: notifyType,
         },
       });
 
-      req.session.tokenId = result.id;
-      req.session.token = accessToken;
+      let message = "";
+      switch (notifyType) {
+        case NotifyType.FIRST_YEAR:
+          message = "1年生向けのお知らせを通知します。";
+          break;
+        case NotifyType.SECOND_YEAR:
+          message = "2年生向けのお知らせを通知します。";
+          break;
+        case NotifyType.ALL:
+        default:
+          message = "すべてのお知らせを通知します。";
+          break;
+      }
 
-      // CSRFトークンを生成
-      const csrfToken = generateRandomString();
-      req.session.csrfToken = csrfToken;
+      await notify.notify(
+        req.session.token,
+        `通知設定が完了しました。${message}毎日18時にお知らせを配信します。`
+      );
 
-      return res.view("select.pug", {
-        title: "通知内容の選択",
+      return res.view("finish", {
+        title: "連携完了",
         message:
-          "通知を受け取りたいお知らせの学年を選択してください。本ページで回答せずに閉じると、すべてのお知らせが通知されます。",
-        csrfToken: csrfToken,
+          "連携しました。お知らせは毎日18時に配信されます。このページは閉じて問題ありません。",
+        isHideLink: true,
       });
     } catch (error) {
-      console.error("Error saving token to database: " + error);
-      return res.status(500).view("finish.pug", {
+      console.error("Error updating notify_type to database: " + error);
+      return res.status(500).view("finish", {
         title: "エラー",
         message:
-          "連携情報を保存するのに失敗しました。しばらく時間をおいてもう一度お試しください。",
+          "通知内容を保存するのに失敗しました。すべてのお知らせが通知されます。希望しない場合は、一度登録を解除してもう一度登録し直してください。",
       });
     }
   }
-});
+);
 
-const finish_schema = {
-  schema: {
-    body: Type.Object({
-      notify_type: Type.Enum(NotifyType),
-      _csrf: Type.String(),
-    }),
-  },
-};
-
-app.post("/finish", finish_schema, async (req, res) => {
-  if (req.body._csrf !== req.session.csrfToken) {
-    return res.status(400).view("error", {
-      message: "CSRF verification failed",
-    });
-  }
-
-  if (req.session.tokenId === undefined || req.session.token === undefined) {
-    return res.status(400).view("error", {
-      message: "Invalid session",
-    });
-  }
-
-  const notifyType = req.body.notify_type;
-  try {
-    // 通知設定を反映
-    await prisma.line_notify_tokens.update({
-      where: {
-        id: req.session.tokenId,
-      },
-      data: {
-        notify_type: notifyType,
-      },
-    });
-
-    let message = "";
-    switch (notifyType) {
-      case NotifyType.FIRST_YEAR:
-        message = "1年生向けのお知らせを通知します。";
-        break;
-      case NotifyType.SECOND_YEAR:
-        message = "2年生向けのお知らせを通知します。";
-        break;
-      case NotifyType.ALL:
-      default:
-        message = "すべてのお知らせを通知します。";
-        break;
-    }
-
-    await notify.notify(
-      req.session.token,
-      `通知設定が完了しました。${message}毎日18時にお知らせを配信します。`
-    );
-
-    return res.view("finish", {
-      title: "連携完了",
-      message:
-        "連携しました。お知らせは毎日18時に配信されます。このページは閉じて問題ありません。",
-      isHideLink: true,
-    });
-  } catch (error) {
-    console.error("Error updating notify_type to database: " + error);
-    return res.status(500).view("finish", {
-      title: "エラー",
-      message:
-        "通知内容を保存するのに失敗しました。すべてのお知らせが通知されます。希望しない場合は、一度登録を解除してもう一度登録し直してください。",
-    });
-  }
-});
-
+const port = parseInt(process.env.PORT || "3000");
 app.listen({ port }, (err) => {
   if (err) throw err;
-  console.log(`App is running on port ${port}`);
 });
