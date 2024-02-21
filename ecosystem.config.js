d4cb726af93d7e09292095e118cd53be5829afe4
seieric <30688855+seieric@users.noreply.github.com>
2024-02-21 23:41:01 +0900

    fix: stagingとproductionの競合を避ける

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,31 @@
+const params = {
+  name: "line-notify-utokyo-zenki",
+  instances: 4,
+  exec_mode: "cluster",
+}
+
+if(process.argv.indexOf('--env') !== -1) {
+  params.name = "ut-notify-stg"
+  params.instances = 1;
+  params.exec_mode = "fork";
+}
+
 module.exports = {
   apps: [
     {
-      name: "line-notify-utokyo-zenki",
+      name: params.name,
       script: "./dist/app.js",
-      env_production: {
+      env: {
         NODE_ENV: "production",
+      },
+      env_staging: {
+        NODE_ENV: "staging",
       },
       env_development: {
         NODE_ENV: "development",
       },
-      instances: 4,
-      exec_mode: "cluster",
+      instances: params.instances,
+      exec_mode: params.exec_mode,
       max_memory_restart: "150M",
     },
   ],
